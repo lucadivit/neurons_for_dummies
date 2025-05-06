25c5b72b0d988fc1964839a849608ef1aee29446
luca.divita <luca.divita@cy4gate.com>
2025-05-06 16:44:42 +0200

    aggiunto codice per incapsulamento

--- run_simple_perceptron.py
+++ run_simple_perceptron.py
@@ -7,7 +7,7 @@ p = Perceptron(dimension=2, activation=Heaviside())
 p.weights = np.array([0.5, 0.5])
 p.bias = 1.
 
-cases = [[0, 0], [0, 1], [1, 0], [1, 1]]
+cases = [[0, 1], [1, 0], [0, 0], [1, 1]]
 true = [0, 0, 0, 1]
 for i, (case, y) in enumerate(zip(cases, true)):
     prediction = p(inputs=np.array(case))
